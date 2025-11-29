534c963af409db60932ca3ba532f09e52077c7ad
AbelChiwandire <achiwandire1@byupathway.edu>
2025-11-29 19:43:39 +0200

    mobile centered

--- chamber/scripts/modules/placesHandler.mjs
+++ chamber/scripts/modules/placesHandler.mjs
@@ -9,17 +9,20 @@ export function displayPlaces() {
 
         const title = document.createElement('h2');
         title.textContent = place.title;
+        title.classList.add('align-text-center', 'text-align-left');
 
         const address = document.createElement('address');
         address.textContent = place.address;
+        address.classList.add('align-text-center', 'text-align-left');
 
         const description = document.createElement('p');
         description.textContent = place.description;
+        description.classList.add('align-text-center', 'text-align-left');
 
         const image = document.createElement('img');
         image.src = `images/${place.image_url}`;
         image.alt = place.name;
-        image.classList.add('contrast');
+        image.classList.add('contrast', 'center-block');
 
         div.append(title, address, description, image);
         container.appendChild(div);
